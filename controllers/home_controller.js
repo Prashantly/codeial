001c3f8b7299e327657b2b7cab6a3037a18d3ca6
Prashant <prashantyallatti79968@gmail.com>
2023-02-24 21:08:36 +0530

    changes in controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,9 @@
 module.exports.home=function(req,res){
 
-    return res.send("<h1>Express is up for codeial</h1>");
+    // return res.send("<h1>Express is up for codeial</h1>");
+
+    return res.render("home",{
+
+        title:"Home"
+    })
 }
