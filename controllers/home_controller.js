9202e239d8e204a46fd6869ed49d473045686845
Prashant <prashantyallatti79968@gmail.com>
2023-03-06 20:41:23 +0530

    create schema for comment, add comments to database and populate commments using nested population on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,7 +14,16 @@ module.exports.home=function(req,res){
     // })
 
     //populate the user of each posts
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+
+        path : 'comments',
+        populate : {
+            path : 'user'
+        }
+    })
+    .exec(function(err,posts){
 
         return res.render('home',{
 
