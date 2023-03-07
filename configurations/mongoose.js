b6fb9e4b6b8c8fbdd56d4af0eb80410b03a67ec8
Prashant <prashantyallatti79968@gmail.com>
2023-03-07 18:52:25 +0530

    delete a post and delete comment

--- configurations/mongoose.js
+++ configurations/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 //provide connection to database
-mongoose.connect("mongodb://127.0.0.1:27017/codeial_development");
+mongoose.connect("mongodb://127.0.0.1:27017/codeial_dev");
 
 const db = mongoose.connection;
 
--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,7 @@ const commentController = require("../controllers/comments_controller");
 
 
 router.post('/create',passport.checkAuthentication,commentController.create);
+router.get('/destroy/:id',passport.checkAuthentication,commentController.destroy);
 
 
 module.exports = router;
