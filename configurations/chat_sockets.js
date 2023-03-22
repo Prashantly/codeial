9986d9779810cb0dcc6615ddfacc03b21ff7ab7e
Prashant <prashantyallatti79968@gmail.com>
2023-03-22 22:50:21 +0530

    initial connection observer and subscriber

--- configurations/chat_sockets.js
+++ configurations/chat_sockets.js
@@ -1,4 +1,22 @@
 
-module.exports.chatSockets = function(chatServer){
+module.exports.chatSockets = function(socketServer){
+
+    let io = require('socket.io')(socketServer,{
+
+        cors : {
+            origin : "http://localhost:8000",
+            methods: ["GET", "POST"]
+        }
+    });
+
+    io.sockets.on('connection',function(socket){
+        console.log('new connection received.',socket.id);
+
+        socket.on('disconnect',function(){
+
+            console.log('socket is disconnected');
+
+        });
+    })
 
 }
