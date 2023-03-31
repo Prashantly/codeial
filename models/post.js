af2263ec9eae8ccc605b64b33898b69db9ccea35
Prashant <prashantyallatti79968@gmail.com>
2023-03-31 20:49:43 +0530

    set up postImage field in postSchema and use multer for storage

--- models/post.js
+++ models/post.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const multer = require('multer');
+const path = require('path');
+
+const IMAGE_PATH = path.join("/uploads/users/postImage");
 
 const postSchema = new mongoose.Schema({
 
@@ -21,11 +25,40 @@ const postSchema = new mongoose.Schema({
     likes : [{
         type : mongoose.Schema.Types.ObjectId,
         ref : "Like"
-    }]
+    }],
+
+    postImage : {
+        type : String,
+    }
 },{
     timestamps : true
 });
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,"..", IMAGE_PATH))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+postSchema.statics.uploadedImage = multer({
+    storage : storage,
+    fileFilter : function(req, res, callback){
+        var ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            callback(new Error('Only images are allowed'), false);
+        }
+        callback(null, true)
+    },
+    limits : {
+        fileSize : 1024 * 1024
+    }
+}).single('postImage');
+
+postSchema.statics.imagePath = IMAGE_PATH;
 
 const Post = mongoose.model('Post',postSchema);
 
